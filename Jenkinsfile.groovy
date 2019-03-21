a2293d1d597c1375d4e62e2e2efbb440e817cc53
kolomyya <andriyzn4@gmail.com>
2019-03-20 19:59:39 -0500

    second commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,18 +1,27 @@
 node {
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Plearse give IP to build the site', name: 'IP', trim: false)])])
-    stage("Install git") {
-        sh "ssh ec2-user@${IP}    sudo yum install  python-pip git -y"
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give IP to build a site', name: 'IP', trim: true)])])
+    stage("Install git"){
+        sh "ssh    ec2-user@${IP}         sudo yum install git python-pip  -y"
     }
-    stage("Clone repo") {
-        git 'https://github.com/kolomyya/Flaskex.git'
+    stage("Clone a repo"){
+        git 'https://github.com/farrukh90/Flaskex.git'
     }
-    stage("Copy files") {
-        sh  "scp  -r * ec2-user@${IP}:/tmp/"
+    stage("Run App"){
+        sh "ssh    ec2-user@${IP}       sudo mkdir  /flaskex 2> /dev/null"
     }
-    stage("Intall requirements") {
-        sh "ssh ec2-user@${IP}    sudo pip install -r /tmp/requirements.txt"
+    stage("Copy files"){
+        sh "scp -r *   ec2-user@${IP}:/home/ec2-user/"
     }
-    stage("Run App") {
-        sh "ssh ec2-user@${IP}   python /tmp/app.py"
+    stage("Move files to /flaskex"){
+        sh "ssh    ec2-user@${IP}      sudo mv  /home/ec2-user/*  /flaskex/"
+    }
+    stage("Install requirements"){
+        sh "ssh    ec2-user@${IP}      sudo pip install -r /flaskex/requirements.txt"
+    }
+    stage("move service to /etc"){
+        sh "ssh    ec2-user@${IP}      sudo mv /flaskex/flaskex.service /etc/systemd/system"
+    }
+    stage("Start service"){
+        sh "ssh    ec2-user@${IP}       sudo systemctl start flaskex"
     }
 }
